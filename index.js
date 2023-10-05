18e768c4a04b2f5a4df88cee323247629dc8758e
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-05 14:37:02 +0100

    first part of display functioning

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// eventually these three variables will be linked to an event listenre to listen for each button being clicked
 let numberOne = "";
 let numberTwo = "";
 let operator = "";
+let displayValue = "";
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -20,29 +20,40 @@ function divide(numberOne, numberTwo) {
   return numberOne / numberTwo;
 }
 
-function operate(numberOne, numberTwo, operator) {
+function calculateResult(numberOne, numberTwo, operator) {
   switch (operator) {
     case "+":
-      add(numberOne, numberTwo);
-      break;
+      return add(numberOne, numberTwo);
     case "-":
-      subtract(numberOne, numberTwo);
-      break;
+      return subtract(numberOne, numberTwo);
     case "*":
-      mulitply(numberOne, numberTwo);
-      break;
+      return mulitply(numberOne, numberTwo);
     case "/":
-      divide(numberOne, numberTwo);
-      break;
+      return divide(numberOne, numberTwo);
     default:
       "Invalid operator";
   }
+}
+
+const operatorArray = ["+", "-", "*", "/"];
+operatorArray.forEach((operator) => {
+  createButton(operator, "operators");
+});
+
+for (let i = 0; i <= 9; i++) {
+  createButton(i.toString(), "digits");
 }
 
 function createButton(label, buttonType) {
   const buttonDiv = document.getElementsByClassName(buttonType)[0];
   const button = document.createElement("button");
   button.innerText = label;
+  button.style.fontSize = "25px";
+  button.style.color = "white";
+  button.style.borderRadius = "10px";
+  button.style.width = "30px";
+  button.classList.add(buttonType);
+
   if (buttonType === "operators") {
     button.style.backgroundColor = "grey";
     button.style.color = "black";
@@ -50,20 +61,65 @@ function createButton(label, buttonType) {
     button.style.backgroundColor = "blue";
     button.style.color = "white";
   }
-  button.style.fontSize = "25px";
-  button.style.color = "white";
-  button.style.borderRadius = "10px";
-  button.style.width = "30px";
 
   buttonDiv.appendChild(button);
+
+  button.addEventListener("click", function () {
+    handleButtonClick(label);
+  });
 }
 
-const operatorArray = ["+", "-", "*", "/"];
-
-for (let i = 0; i <= 9; i++) {
-  createButton(i.toString(), "digits");
-}
-
-operatorArray.forEach((operator) => {
-  createButton(operator, "operators");
+document.querySelector(".equals").addEventListener("click", function () {
+  calculateResult();
 });
+
+document.querySelector(".clear").addEventListener("click", function () {
+  clearCalculator();
+});
+
+function handleButtonClick(buttonLabel) {
+  if (buttonLabel === "Clear") {
+    clearCalculator();
+  } else if (buttonLabel === "=") {
+    calculateResult(); // ✨✨need to pass in the arguments
+  } else {
+    updateDisplay(buttonLabel);
+  }
+}
+
+function clearCalculator() {
+  numberOne = "";
+  numberTwo = "";
+  operator = "";
+  displayValue = "";
+  const display = document.querySelector(".calculator-display");
+  display.innerText = displayValue;
+}
+
+//update display
+// when a digit button is pressed update the DisplayValue
+// when an operator Button is pressed update the numberOne value with the displayValue
+
+function updateDisplay(buttonLabel) {
+  const display = document.querySelector(".calculator-display");
+  if (isDigit(buttonLabel)) {
+    displayValue += buttonLabel;
+  } else if (isOperator(buttonLabel)) {
+    numberOne = displayValue;
+    operator = buttonLabel;
+    displayValue = "";
+    console.log(numberOne);
+    console.log(operator);
+  }
+  display.innerText = displayValue;
+}
+
+function isDigit(buttonLabel) {
+  return !isNaN(buttonLabel);
+}
+
+function isOperator(buttonLabel) {
+  return operatorArray.includes(buttonLabel);
+}
+
+clearCalculator(); // clears the calculator on page load
